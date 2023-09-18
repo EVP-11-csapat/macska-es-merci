732a01e813a193ca6b3c483ec8d7f82c17f4256f
Németh Tamás <34923997+Nemeth-Tamas@users.noreply.github.com>
2023-09-18 09:45:19 +0200

    Moved decryption to own api

--- secret.js
+++ secret.js
@@ -1,4 +1,4 @@
-import CryptoJS from 'crypto-js';
+import axios from 'axios';
 
 let randomImage;
 let randomImage2;
@@ -15,65 +15,94 @@ fetch('./secret.txt').then((response) => {
         return response.text();
     }).then((data2) => {
         let encRandomImage2 = data2;
-        const iv = CryptoJS.enc.Hex.parse(import.meta.env.VITE_DECRYPTION_IV);
-        const key = CryptoJS.enc.Hex.parse(import.meta.env.VITE_DECRYPTION_KEY);
-
-        randomImage = CryptoJS.AES.decrypt(encRandomImage, key, { iv: iv }).toString(CryptoJS.enc.Utf8);
-        randomImage2 = CryptoJS.AES.decrypt(encRandomImage2, key, { iv: iv }).toString(CryptoJS.enc.Utf8);
         fetch('./secret3.txt').then((response) => {
             return response.text();
         }).then((data3) => {
-            secretCode = CryptoJS.AES.decrypt(data3, key, { iv: iv }).toString(CryptoJS.enc.Utf8);
+            let encSecretCode = data3;
             fetch('./secret4.txt').then((response) => {
                 return response.text();
             }).then((data4) => {
-                secretCode2 = CryptoJS.AES.decrypt(data4, key, { iv: iv }).toString(CryptoJS.enc.Utf8);
+                let encSecretCode2 = data4;
+                let url = 'https://macskamerci.ntsexp.site/decrypt';
+
+                axios.post(url, { data: encRandomImage })
+                    .then((response) => {
+                        if (response.status === 200) {
+                            const { data } = response;
+                            randomImage = data;
+                        }
+                    })
+                axios.post(url, { data: encRandomImage2 })
+                    .then((response) => {
+                        if (response.status === 200) {
+                            const { data } = response;
+                            randomImage2 = data;
+                        }
+                    })
+                axios.post(url, { data: encSecretCode })
+                    .then((response) => {
+                        if (response.status === 200) {
+                            const { data } = response;
+                            secretCode = data;
+                        }
+                    })
+                axios.post(url, { data: encSecretCode2 })
+                    .then((response) => {
+                        if (response.status === 200) {
+                            const { data } = response;
+                            secretCode2 = data;
+                        }
+                    })
             });
         });
     });
 });
 
 export function init() {
-    document.addEventListener('keydown', function (event) {
-        const image = document.querySelector('.cat');
-        const image2 = document.querySelector('.car');
-        if (event.key === atob(secretCode).charAt(0)) {
-            let index = 1;
-            event.preventDefault();
+    axios.get('https://macskamerci.ntsexp.site/check').then((response) => {
+        if (response.status === 200) {
             document.addEventListener('keydown', function (event) {
-                if (event.key === atob(secretCode).charAt(index)) {
-                    index++;
-                    if (index === atob(secretCode).length) {
-                        image.src = randomImage;
-                        firstActive = true;
-                    }
-                } else {
-                    index = 0;
+                const image = document.querySelector('.cat');
+                const image2 = document.querySelector('.car');
+                if (event.key === atob(secretCode).charAt(0)) {
+                    let index = 1;
+                    event.preventDefault();
+                    document.addEventListener('keydown', function (event) {
+                        if (event.key === atob(secretCode).charAt(index)) {
+                            index++;
+                            if (index === atob(secretCode).length) {
+                                image.src = randomImage;
+                                firstActive = true;
+                            }
+                        } else {
+                            index = 0;
+                        }
+                    });
+                }
+
+                if (event.key === atob(secretCode2).charAt(0) && firstActive) {
+                    let index = 1;
+                    event.preventDefault();
+                    document.addEventListener('keydown', function (event) {
+                        if (event.key === atob(secretCode2).charAt(index)) {
+                            index++;
+                            if (index === atob(secretCode2).length) {
+                                image2.src = randomImage2;
+                                secondActive = true;
+                            }
+                        } else {
+                            index = 0;
+                        }
+                    });
+                }
+
+                if (event.key === 'r' && firstActive && secondActive) {
+                    firstActive = false;
+                    secondActive = false;
+                    image.src = './img/macska.webp'
+                    image2.src = './img/merci.webp'
                 }
             });
-        }
-
-        if (event.key === atob(secretCode2).charAt(0) && firstActive) {
-            let index = 1;
-            event.preventDefault();
-            document.addEventListener('keydown', function (event) {
-                if (event.key === atob(secretCode2).charAt(index)) {
-                    index++;
-                    if (index === atob(secretCode2).length) {
-                        image2.src = randomImage2;
-                        secondActive = true;
-                    }
-                } else {
-                    index = 0;
-                }
-            });
-        }
-
-        if (event.key === 'r' && firstActive && secondActive) {
-            firstActive = false;
-            secondActive = false;
-            image.src = './img/macska.webp'
-            image2.src = './img/merci.webp'
         }
     });
 }
